32253012a578bafbf8d17623d7e4734ca32cf8e7
edson melo <epm.tes@gmail.com>
2025-06-12 12:21:08 -0300

    Atualização

--- banco_de_dados/db.js
+++ banco_de_dados/db.js
@@ -1,16 +1,12 @@
 // Importa o módulo 'sqlite3', que permite usar um banco de dados SQLite no Node.js
-// O método 'verbose()' ativa mensagens extras no console (mensagens de erro mais detalhadas)
 const sqlite3 = require('sqlite3').verbose();
 
 // Cria ou abre o banco de dados chamado 'medicamentos.db' dentro da pasta 'banco_de_dados'
-// Se o arquivo não existir, ele será criado automaticamente
 const banco = new sqlite3.Database('./banco_de_dados/medicamentos.db');
 
-// Usa o método 'serialize' para garantir que os comandos SQL sejam executados em ordem, um após o outro
-// Evita conflitos que podem acontecer quando várias operações tentam acessar o banco ao mesmo tempo
+// Método 'serialize' para garantir que os comandos SQL sejam executados em ordem, um após o outro
 banco.serialize(() => {
     // Cria a tabela 'medicamentos' se ela ainda não existir
-    // Esta tabela terá os campos: id, nome, dosagem e horário
     banco.run(`
         CREATE TABLE IF NOT EXISTS medicamentos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
--- server.js
+++ server.js
@@ -2,26 +2,21 @@
 const express = require('express');
 
 // Cria a aplicação principal (servidor) usando Express
-// A constante 'aplicacao' será usada para configurar o servidor, rotas e outras funções
 const aplicacao = express();
 
 // Importa as rotas relacionadas a medicamentos
 const rotasMedicamentos = require('./rotas/medicamentos');
 
-// Define a porta onde o servidor vai escutar as requisições (como uma entrada de acesso)
-// Pode ser alterada para outra porta, como 8080 ou 5000, se necessário
+// Define a porta onde o servidor vai escutar as requisições
 const porta = 3000;
 
 // Middleware, permite que o servidor entenda dados enviados em formato JSON no corpo das requisições
-// Isso é necessário para que possamos ler os dados enviados com POST, PUT, etc.
 aplicacao.use(express.json());
 
 // Conecta as rotas de medicamentos à aplicação principal
-// URL "/medicamentos", será direcionada para as rotas definidas em rotas/medicamentos.js
 aplicacao.use('/medicamentos', rotasMedicamentos);
 
 // Inicia o servidor e faz com que ele comece a escutar na porta definida
-// Quando o servidor estiver funcionando, exibe a mensagem no terminal
 aplicacao.listen(porta, () => {
   console.log(`Servidor rodando em http://localhost:${porta}`);
 });
